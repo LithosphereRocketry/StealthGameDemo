22ddc16d2395e97b2f2beb91bcf79ea4fb7bcd00
LithosphereRocketry <greatmaharg@gmail.com>
2023-05-24 02:56:18 -0400

    Vector debug tools

--- include/SDLDebug.h
+++ include/SDLDebug.h
@@ -1,9 +1,28 @@
 #include <SDL2/SDL.h>
+#include "VectorMath.h"
 
 /**
  * Draws a vector based on the given viewport information, with pretty arrow
 */
-void drawVector(SDL_Renderer* rend, Vector<float, 2> vec, Vector<float, 2> origin,
-                float camx, float camy, float zoom) {
-    // NYI
+SDL_Point toScreenSpace(SDL_Renderer* rend, Vector<float, 2> pos, float px_unit) {
+    int w, h;
+    SDL_GetRendererOutputSize(rend, &w, &h);
+    Vector<float, 2> scrctr = {w/2, h/2};
+    Vector<float, 2> posflip = {pos[0], -pos[1]};
+    Vector<float, 2> posscr = posflip * px_unit + scrctr;
+
+    return {int(posscr[0]), int(posscr[1])};
+}
+
+void drawVector(SDL_Renderer* rend, Vector<float, 2> vec, Vector<float, 2> origin, Vector<float, 2> campos, float px_unit) {
+    Vector<float, 2> corner1 = origin + vec*0.9 + vec.orthogonal()*0.1; 
+    Vector<float, 2> corner2 = origin + vec*0.9 - vec.orthogonal()*0.1;
+    SDL_Point points[] = {
+        toScreenSpace(rend, origin-campos, px_unit),
+        toScreenSpace(rend, origin+vec-campos, px_unit),
+        toScreenSpace(rend, corner1-campos, px_unit),
+        toScreenSpace(rend, corner2-campos, px_unit),
+        toScreenSpace(rend, origin+vec-campos, px_unit)
+    };
+    SDL_RenderDrawLines(rend, points, 5);
 }
